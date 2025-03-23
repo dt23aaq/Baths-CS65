fdcadce54476328061cdd41e7b5c25461985aaba
sf23abk <lekanfabiyi@yahoo.com>
2025-03-23 16:07:58 +0000

    updating names of all

---
+++ mavenproject2/src/main/java/te/mavenproject2/Mavenproject2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package te.mavenproject2;
+
+/**
+ *
+ * @author lekan
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
